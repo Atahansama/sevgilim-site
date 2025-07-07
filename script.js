1ea6879694e333f2b8c3745cb7e875f2ccf30bf8
Atahan <aertopoglu@gmail.com>
2025-07-07 04:33:14 +0300

    İlk versiyon: proje dosyaları

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+const music = document.getElementById('music');
+
+document.body.addEventListener('click', () => {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+});
